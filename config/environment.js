913230670de0fa473ff907fab968571a67bea2be
Summer <sbrochtrup@gmail.com>
2015-09-30 13:43:00 -0700

    Add firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'learn-piano',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://learn-piano-project.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
